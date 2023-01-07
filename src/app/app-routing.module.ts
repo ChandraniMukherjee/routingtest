4020e5e4527c9fcf2b1b11cd30658f727143943c
ChandraniMukherjee <mukherjee.debjani75@gmail.com>
2023-01-07 14:21:08 +0530

    do

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"chandranihome",component:HomeComponent},
+  {path:"chandraniproducts",component:ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const chandranirr=[HomeComponent,ProductsComponent];
